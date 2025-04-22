8e62bb715016a721d24aa0039e98663563d66d1d
wg <wgkim9310@gmail.com>
2025-04-22 20:36:55 +0900

    BCcrypt Setting

--- src/main/java/com/side/mylist/config/SecurityConfig.java
+++ src/main/java/com/side/mylist/config/SecurityConfig.java
@@ -4,11 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -36,6 +46,7 @@ public class SecurityConfig {
 
 
         return httpSecurity.build();
+
     }
 
 }
